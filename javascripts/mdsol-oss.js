e6e6bd2f50d594060513fdab695b937808369992
Bernardo Zuniga <bzuniga@mdsol.com>
2013-06-15 10:04:40 -0400

    add links for repos

--- javascripts/mdsol-oss.js
+++ javascripts/mdsol-oss.js
@@ -1,6 +1,6 @@
 ;(function($) {
   console.log('init');
-  $.get('https://api.github.com/orgs/mdsol/repos?type=sources', function(data) {
+  $.get('https://api.github.com/orgs/mdsol/repos?type=forks', function(data) {
     console.log(data.length, data)
   })
 }(jQuery));

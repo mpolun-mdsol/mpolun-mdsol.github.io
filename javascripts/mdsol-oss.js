11b6dfaa328dd642195531592a983a93813c895a
Bernardo Zuniga <bzuniga@mdsol.com>
2013-07-12 17:48:57 -0400

    add gh3 lib

--- javascripts/mdsol-oss.js
+++ javascripts/mdsol-oss.js
@@ -1,11 +1,17 @@
 ;(function($) {
-  var mdsol = new Gh3.User("mdsol");
+  var mdsol = new Gh3.User("mdsol"),
+      repos = new Gh3.Repositories(mdsol);
   mdsol.fetch(function(err, resUser) {
     if(err) {
       throw "error"
     }
-    console.log(mdsol, resUser);  
   });
-  
+
+  repos.fetch(function(err, res) {
+    if(err) {
+      throw "err"
+    }
+    console.log(res, repos)
+  });
  
 }(jQuery));

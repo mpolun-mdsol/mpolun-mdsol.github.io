e334c72929f9e73d2341b65a0f688ac74ae80092
Bernardo Zuniga <bzuniga@mdsol.com>
2013-07-12 18:52:35 -0400

    visual update

--- javascripts/mdsol-oss.js
+++ javascripts/mdsol-oss.js
@@ -1,17 +1,3 @@
 ;(function($) {
-  var mdsol = new Gh3.User("mdsol"),
-      repos = new Gh3.Repositories(mdsol);
-  mdsol.fetch(function(err, resUser) {
-    if(err) {
-      throw "error"
-    }
-  });
 
-  repos.fetch(function(err, res) {
-    if(err) {
-      throw "err"
-    }
-    console.log(res, repos)
-  });
- 
 }(jQuery));

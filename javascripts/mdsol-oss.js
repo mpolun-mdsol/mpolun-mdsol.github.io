3d3eeab0169a209f301d00b183cb41e804985c78
Bernardo Zuniga <bzuniga@mdsol.com>
2013-07-12 23:50:22 -0400

    grid

--- javascripts/mdsol-oss.js
+++ javascripts/mdsol-oss.js
@@ -1,3 +1,22 @@
 ;(function($) {
+  console.log('doc ready');
+  var cols = 12,
+      rows = ['aa','bb','cc','dd','ee','ff','gg'];
+
+  for(var i = 0; i < cols; i++) {
+    console.log("make col");
+    var $ul = $('<ul />', {
+      'class': 'column clearfix',
+      'id': 'col_'+i
+    });
+    var periodicBoxes = "";
+    for(var j = 0, len = rows.length; j < len; j++) {
+      console.log('make row', '<li>' + rows[j] + '</li>');
+      periodicBoxes += '<li><a href="">' + rows[j] + '</a></li>';
+    }
+    console.log($ul);
+    $ul.html(periodicBoxes);
+    $ul.appendTo('#periodic');
+  }
 
 }(jQuery));

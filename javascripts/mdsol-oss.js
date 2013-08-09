a448c81fc3ce33ce9798eb86691094f228f70170
Bernardo Zuniga <bzuniga@mdsol.com>
2013-08-09 16:53:14 -0400

    add grid and hovers

--- javascripts/mdsol-oss.js
+++ javascripts/mdsol-oss.js
@@ -1,25 +1,29 @@
 ;(function($) {
-  console.log('doc ready');
-  var cols = 12,
-      rows = 7,
-      possible = 'abcdefghijklmnopqrstuvwxyz';
+  // console.log('doc ready');
+  // var cols = 12,
+  //     rows = 4,
+  //     possible = 'abcdefghijklmnopqrstuvwxyz';
 
-  for(var i = 0; i < cols; i++) {
-    console.log("make col");
-    var $ul = $('<ul />', {
-      'class': 'column clearfix',
-      'id': 'col_'+i
-    });
-    var periodicBoxes = "";
-    for(var j = 0; j < rows; j++) {
-      console.log('make row', '<li>' + rows[j] + '</li>');
-      var firstLetter =  possible.charAt(Math.floor(Math.random() * possible.length)),
-          secondLetter =  possible.charAt(Math.floor(Math.random() * possible.length));
-      periodicBoxes += '<li><a href="">' + firstLetter + secondLetter  + '</a></li>';
-    }
-    console.log($ul);
-    $ul.html(periodicBoxes);
-    $ul.appendTo('#periodic');
-  }
-
+  // for(var i = 0; i < cols; i++) {
+  //   console.log("make col");
+  //   var $ul = $('<ul />', {
+  //     'class': 'column clearfix',
+  //     'id': 'col_'+i
+  //   });
+  //   var periodicBoxes = "";
+    
+  //   for(var j = 0; j < rows; j++) {
+  //     console.log('make row', '<li>' + rows[j] + '</li>');
+  //     var firstLetter =  possible.charAt(Math.floor(Math.random() * possible.length)),
+  //         secondLetter =  possible.charAt(Math.floor(Math.random() * possible.length));
+  //     periodicBoxes += '<li><a href="">' + firstLetter + secondLetter  + '</a></li>';
+  //   }
+  //   console.log($ul);
+  //   $ul.html(periodicBoxes);
+  //   $ul.appendTo('#periodic');
+  // }
+  $('#periodic a').popover({
+    'trigger': 'hover',
+    'html':'true'
+  })
 }(jQuery));
